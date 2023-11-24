7c896e9a26b238988d4cea5757e943000e3598d6
dogeprojects <>
2023-11-24 16:13:20 +0300

    - Refactoring

--- app/src/androidTest/java/com/pricecomparation/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pricecomparation/ExampleInstrumentedTest.java
@@ -40,7 +40,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-       // Context appContext = InstrumentationRegistry.getTargetContext();
+        // Context appContext = InstrumentationRegistry.getTargetContext();
 
       //  assertEquals("com.priceperkilo", appContext.getPackageName());
     }
